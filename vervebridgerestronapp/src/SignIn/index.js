2472fddb6b5611210cb4f2a280c7cff7af2c0071
NithinAmbati <nithinambati9@gmail.com>
2024-09-21 10:04:43 +0530

    Done

--- vervebridgerestronapp/src/SignIn/index.js
+++ vervebridgerestronapp/src/SignIn/index.js
@@ -48,7 +48,7 @@ class Signin extends Component {
         body: JSON.stringify({ username, email, password }),
       };
 
-      const url = `${mainUrl}/login/student`;
+      const url = `${mainUrl}/api/user/register`;
 
       // Fetch API request
       const response = await fetch(url, options);
